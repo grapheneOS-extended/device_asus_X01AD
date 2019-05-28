4899035168fdd09f8c9756202585b98ff33a203e
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    X01AD: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,7 +155,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
