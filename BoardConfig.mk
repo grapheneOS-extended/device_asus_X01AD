d43d5d7be1ae857165c62b6f39304dd0078eb22e
Erfan Abdi <erfangplus@gmail.com>
2020-11-11 22:40:10 +0330

    X01AD: Fix typo in console kernel parameter
    Change-Id: Ia38914588a0b0e3bc30c51df9a7b8681f5bf9d76

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_BOOTLOADER_BOARD_NAME := SDM632
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
