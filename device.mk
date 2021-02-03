02c746a5f16ffd48596a1eab2424c45d2d9a9909
danascape <saalimquadri1@gmail.com>
2021-02-03 05:53:05 +0100

    X01AD: Update aapt conf * better

--- device.mk
+++ device.mk
@@ -28,7 +28,7 @@ TARGET_SCREEN_WIDTH := 720
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=288
 
