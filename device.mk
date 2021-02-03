56a7a2d64fa4d666b607ef2fbc4e5193a8a2c706
danascape <saalimquadri1@gmail.com>
2021-02-03 05:54:06 +0100

    X01AD: Override lcd density * nothing else was working

--- device.mk
+++ device.mk
@@ -29,6 +29,8 @@ TARGET_SCREEN_WIDTH := 720
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=288
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
